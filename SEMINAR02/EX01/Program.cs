72e823cea7563c9edb2541c7161488edc141de16
Ustas <ustasplohovski@gmail.com>
2022-11-24 11:27:39 +0300

    Текущее решение в примере EX01

--- SEMINAR02/EX01/Program.cs
+++ SEMINAR02/EX01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int num = new Random().Next(10,100);
+Console.WriteLine($"Сгенерированно случайное число {num}");
+int max = num / 10;
+if (num%10 > max)  max = num%10;
+Console.WriteLine($"Наибольшая цифра {max}");
+
+for (int i=0;i<=num;i++) Console.Write($"{i}, ");
+
+// Задача 2 - из двух чисел найти минимальное и максимальное
+
+Console.Write("Enter the first any integer:");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second any integer:");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)

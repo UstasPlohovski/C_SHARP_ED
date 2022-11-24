c98dcc8a9e0f82353f239daf436a591cb83aaa6e
Ustas <ustasplohovski@gmail.com>
2022-11-24 17:53:02 +0300

    Добавлены примеры задачь EX05_1, EX06

---
+++ SEMINAR02/EX05_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача - определить третью цифру слева в любом произвольном заданном целом числе.
+//    (решение через цикл)
+
+Console.WriteLine("Enter number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int CurrentNumber = 0, Remainder = 0;
+
+if (Number > 99)
+{
+    CurrentNumber = Number;
+    while (CurrentNumber >= 1000)
+    {
+        CurrentNumber /= 10;
+        Remainder = CurrentNumber % 10;
+    }
+    Console.WriteLine($"The third number from the left is {Remainder}");
+}
+else
+{
+Console.WriteLine("The thidr number isn't in the number");
+}

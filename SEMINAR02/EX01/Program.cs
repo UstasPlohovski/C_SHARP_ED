492d60ac2a1dabdbd99057297db4ba15d31cbe6c
Ustas <ustasplohovski@gmail.com>
2022-11-24 15:55:35 +0300

    Добавлеы примеры задачь EX02 - EX05

--- SEMINAR02/EX01/Program.cs
+++ SEMINAR02/EX01/Program.cs
@@ -1,19 +1,7 @@
-﻿// int num = new Random().Next(10,100);
-Console.WriteLine($"Сгенерированно случайное число {num}");
+﻿// Задача - найти наибольшую цифру в двухзнвчном числе.
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Сгенерировано случайное число {num}");
 int max = num / 10;
-if (num%10 > max)  max = num%10;
-Console.WriteLine($"Наибольшая цифра {max}");
-
-for (int i=0;i<=num;i++) Console.Write($"{i}, ");
-
-// Задача 2 - из двух чисел найти минимальное и максимальное
-
-Console.Write("Enter the first any integer:");
-
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter the second any integer:");
-
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if (number1 > number2)
+if (num % 10 > max) max = num % 10;
+Console.WriteLine($"Наибольшая цифра в этом числе это {max}");
---
+++ SEMINAR02/EX03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача - определить является ли второе число кратным первому
+
+Console.WriteLine("Enter two numbers:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n2 % n1 == 0) Console.WriteLine("The second number is a multiple of the first.");
+else
+{
+    Console.WriteLine("The second number is NOT a multiple of the first.");
+    int r = n2 % n1;
+    Console.WriteLine($"The residue  is {r}");
+}

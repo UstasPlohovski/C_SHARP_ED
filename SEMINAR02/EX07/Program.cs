9e8d337f796c39fc8a77390b2e9da82138d994f8
Ustas <ustasplohovski@gmail.com>
2022-11-25 16:28:09 +0300

    Добавлена задача EX07

--- SEMINAR02/EX07/Program.cs
+++ SEMINAR02/EX07/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача - определить вторую цифру в любом произвольном трехзначном заданном целом числе.
+//    (решение через цикл)
+
+Console.WriteLine("Enter number from 0 to 1000");
+int Number = Convert.ToInt32(Console.ReadLine());
+// int CurrentNumber = 0, 
+int Remainder = 0;
+
+if (Number > 0 && Number < 1000)
+{
+//if (Number > 99)
+//{
+//    CurrentNumber = Number;
+//    while (CurrentNumber >= 1000)
+//    {
+        Number /= 10;
+        Remainder = Number % 10;
+//    }
+    Console.WriteLine($"The third number from the left is {Remainder}");
+//}
+//else
+//{
+}
+else
+{
+Console.WriteLine("You enter the number is not from 0 to 1000");
+}
+
+//}

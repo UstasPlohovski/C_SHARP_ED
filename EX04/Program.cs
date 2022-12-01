cdac1d5c80321146b79d27cc3d258b8151d072b9
Ustas <ustasplohovski@gmail.com>
2022-12-01 23:40:11 +0300

    Добавлены примеры EX04 - 05

---
+++ EX04/Program.cs
@@ -0,0 +1,43 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками, // маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+string text = “— Я думаю, — сказал князь, улыбаясь, — что,”
+● ”ежели бы вас послали вместо нашего милого Винценгероде,”
+● ”вы бы взяли приступом согласие прусского короля.”
+● ”Вы так красноречивы. Вы дадите мне чаю?”;
+
+string Replase(string text, char oldValue, char newValue) 
+{
+string result = String.Empty;
+int length = text.Length; 
+for(inti=0;i<length;i++) 
+{
+    if(text[i] == oldValue) 
+    {
+    result + $”{NewValue}”;
+    }
+else result + $”{str[i]}”; 
+}
+return result 
+}
+string newText = replace(text, ‘ ’, ‘ | ‘); 
+Console.WriteLine(newText);
+
+string Replase(string text, char oldValue, char newValue) 
+{
+string result = String.Empty; 
+int length = text Length; 
+for(inti=0;i<length;i++) 
+{
+if(text[i] == oldValue)
+{
+    result = result + $”{NewValue}”;
+}
+else
+{ 
+    result + result $”{text[i]}”;
+ } 
+ }
+return result 
+}

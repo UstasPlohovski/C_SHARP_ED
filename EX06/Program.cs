64b55d547214f7eb6eef989f35e9bbb15f7e263d
Ustas <ustasplohovski@gmail.com>
2022-12-04 23:15:42 +0300

    Добавлены задачи семинара 3

---
+++ EX06/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координату Х");
+int x  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+int y  = Convert.ToInt32(Console.ReadLine());
+CheckKoord(x, y);
+// if (x>0 && y>0) Console.WriteLine("Это четверть № 1");
+// else if (x>0 && y<0) Console.WriteLine("Это четверть № 4");
+// else if (x<0 && y>0) Console.WriteLine("Это четверть № 2");
+// else if (x<0 && y<0) Console.WriteLine("Это четверть № 3");
+// else Console.WriteLine("Точка находится на оси");
+
+void CheckKoord (int x, int y) // нечего не возвращает, но что-то делает
+{
+    if (x>0 && y>0) Console.WriteLine("Это четверть № 1");
+    else if (x>0 && y<0) Console.WriteLine("Это четверть № 4");
+    else if (x<0 && y>0) Console.WriteLine("Это четверть № 2");
+    else if (x<0 && y<0) Console.WriteLine("Это четверть № 3");
+    else Console.WriteLine("Точка находится на оси");
+}
---
+++ EX08/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве. A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21 
+
+try
+{
+    Console.WriteLine("Введите координату A, два числа сначала по X далее по Y:");
+    int ax = Convert.ToInt32(Console.ReadLine());
+    int ay = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координату B, два числа сначала по X далее по Y:");
+    int bx = Convert.ToInt32(Console.ReadLine());
+    int by = Convert.ToInt32(Console.ReadLine());
+    
+    //double r = distance(ax,ay,bx,by);
+    //Console.WriteLine($"Расстояние между точками A и B: {r}");
+    Console.WriteLine($"Расстояние между точками A и B: {distance(ax,ay,bx,by)}");
+
+}
+catch // если же не получилось выполнить всю программу, то уходим в этот блок и информуруем пользователя
+{
+    Console.WriteLine("Ошибка: Надо было вводить именно целое число цифрами!");
+}
+
+// Функция возвращает число double ----------------------------------------------------
+
+double distance (int ax, int ay, int bx, int by)
+{
+    double d = Math.Sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
+    return d;
+}

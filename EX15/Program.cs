b801f31678e64fb72b9de6f22f0caa42eb7949e6
Ustas <ustasplohovski@gmail.com>
2022-12-07 16:36:10 +0300

    Добавлены задачи 14 и 15

---
+++ EX15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+
+Console.WriteLine("Введите размерность пространства (N): ");        // определяем размерность пространства
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    int[] ArrayA = new int [N];                                     // вводим координаты точки А в виде массива
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        Console.WriteLine($"Введите {(i+1)}ую координату точки A");
+        ArrayA[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    Console.Write("You have new array; [");
+    for (int i = 0; i < (ArrayA.Length-1); i++)
+    {
+        Console.Write($"{ArrayA[i]}; ");
+    }
+    Console.Write($"{ArrayA[^1]}]");

bc0a4decf06f947114216256620fc384ac7aba02
Ustas <ustasplohovski@gmail.com>
2022-12-14 17:46:37 +0300

    Add tasks SEMINAR6.

---
+++ SEMINAR06/TASK42/Program.cs
@@ -0,0 +1,17 @@
+﻿//задача 42 Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
+ 
+Console.WriteLine("Введите число"); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+FromTen(num); 
+ 
+void FromTen(int num) 
+{ 
+    string count = ""; 
+    while (num > 1) 
+    { 
+        count = Convert.ToString(num % 2) + count; 
+        num = num / 2; 
+    } 
+    count = 1 + count; 
+    Console.WriteLine(count); 
+}
